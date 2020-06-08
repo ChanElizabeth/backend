23344c88c938fdde43b94a54d3326b1768c96c1a
Chan Elizabeth Wirawan <elizabethchan@CElizs-MacBook-Pro.local>
2020-06-08 19:40:09 +0700

    test

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -148,7 +148,7 @@ class UserController extends Controller
         }
         Auth::logout();
         return response()->json([
-            'message' => 'Successfully logged out'
+            'message' => $token
             
         ]);
     }
